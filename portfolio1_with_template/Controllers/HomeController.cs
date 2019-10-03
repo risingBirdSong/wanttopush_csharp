c4f60540f9a82a3d08a4bc563a2f6a0ca6e448fc
risingbirdsong <heregoingthere@outlook.com>
2019-10-03 13:24:27 -0700

    Add project files.

---
+++ portfolio1_with_template/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using portfolio1_with_template.Models;
+
+namespace portfolio1_with_template.Controllers
+{
+    public class HomeController : Controller
+    {
+         [HttpGet("")]
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet("projects")]
+        public IActionResult Projects()
+        {
+            return View();
+        }
+
+        [HttpGet("contacts")]
+        public IActionResult Contacts()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ portfolio1_with_template/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace portfolio1_with_template.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
