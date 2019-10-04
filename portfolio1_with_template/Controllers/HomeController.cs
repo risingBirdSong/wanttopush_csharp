bcb02fb2b7c4d9e330f5e696ffcc796a8e42a18f
risingbirdsong <heregoingthere@outlook.com>
2019-10-04 14:22:34 -0700

    working with enums

--- portfolio1_with_template/Controllers/HomeController.cs
+++ portfolio1_with_template/Controllers/HomeController.cs
@@ -36,6 +36,29 @@ namespace portfolio1_with_template.Controllers
         public IActionResult Datetime() {
             return View();
         }
+        [HttpGet("form")]
+        public IActionResult Form()
+        {
+            var myForm = new formModel();
+            return View(myForm);
+        }
+
+        [HttpPost("result")]
+        [ValidateAntiForgeryToken]
+        public IActionResult Result(string Name,string myLocations, string Languages, string Comment)
+        {
+            myLocations outLocation;
+            Languages outLanguage;
+            Enum.TryParse<myLocations>(myLocations, out outLocation);
+            Enum.TryParse<Languages>(Languages, out outLanguage);
+            var myresult = new formModel();
+            myresult.Name = Name;
+            myresult.myLocations = outLocation;
+            myresult.Languages = outLanguage;
+            myresult.Comment = Comment;
+            return View(myresult);
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
---
+++ portfolio1_with_template/Models/formModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace portfolio1_with_template.Models
+{
+
+    public enum myLocations {
+        Seattle,
+        Chicago,
+        Austin,
+    }
+
+    public enum Languages
+    {
+        CSharp,
+        Typescript,
+        Rust,    
+    }
+
+    public class formModel
+    {
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+        [Display(Name = "Our Locations")]
+        public Enum myLocations { get; set; }
+        [Display(Name = "Languages")]
+        public Enum Languages { get; set; }
+        [Display(Name = "Comment")]
+
+        public string Comment { get; set; }
+
+    }
+}
