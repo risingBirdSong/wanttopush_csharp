6847982fb640d5234e6ff70a7e1a33b5727ab245
risingbirdsong <heregoingthere@outlook.com>
2019-10-04 17:32:27 -0700

    working with models

--- portfolio1_with_template/Controllers/HomeController.cs
+++ portfolio1_with_template/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace portfolio1_with_template.Controllers
 {
     public class HomeController : Controller
     {
-         [HttpGet("")]
+        [HttpGet("")]
 
         public IActionResult Index()
         {
@@ -33,37 +33,60 @@ namespace portfolio1_with_template.Controllers
             return View();
         }
         [HttpGet("datetime")]
-        public IActionResult Datetime() {
+        public IActionResult Datetime()
+        {
             return View();
         }
-        [HttpGet("form")]
-        public IActionResult Form()
+
+        [HttpGet("form_norm")]
+        public IActionResult form_norm()
         {
-            var myForm = new formModel();
+            var locations = new List<string> { "Vancouver", "Portland", "Spokane" };
+            var languages = new List<string> { "french", "english", "german" };
+            var myForm = new form_norm();
+            myForm.Location = locations;
+            myForm.FavoriteLanguage = languages;
             return View(myForm);
-        }
-
-        [HttpPost("result")]
-        [ValidateAntiForgeryToken]
-        public IActionResult Result(string Name,string myLocations, string Languages, string Comment)
-        {
-            myLocations outLocation;
-            Languages outLanguage;
-            Enum.TryParse<myLocations>(myLocations, out outLocation);
-            Enum.TryParse<Languages>(Languages, out outLanguage);
-            var myresult = new formModel();
-            myresult.Name = Name;
-            myresult.myLocations = outLocation;
-            myresult.Languages = outLanguage;
-            myresult.Comment = Comment;
-            return View(myresult);
-        }
-
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+      
     }
+        [HttpPost("form_norm")]
+        [ValidateAntiForgeryToken]
+
+        public IActionResult norm_result(form_norm postedForm)
+        {
+
+            return View(postedForm);
+        }
+
+
+    [HttpGet("form")]
+    public IActionResult Form()
+    {
+        var myForm = new formModel();
+        return View(myForm);
+    }
+
+    [HttpPost("result")]
+    [ValidateAntiForgeryToken]
+    public IActionResult Result(string Name, string myLocations, string Languages, string Comment)
+    {
+        myLocations outLocation;
+        Languages outLanguage;
+        Enum.TryParse<myLocations>(myLocations, out outLocation);
+        Enum.TryParse<Languages>(Languages, out outLanguage);
+        var myresult = new formModel();
+        myresult.Name = Name;
+        myresult.myLocations = outLocation;
+        myresult.Languages = outLanguage;
+        myresult.Comment = Comment;
+        return View(myresult);
+    }
+
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
 }
---
+++ portfolio1_with_template/Models/form_norm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace portfolio1_with_template.Models
+{
+    public class form_norm
+    {
+        public string Name { get; set; }
+
+        public List<string> Location { get; set; }
+        public List<string> FavoriteLanguage {get; set;}
+        public string Comment { get; set; }
+
+
+    }
+}
